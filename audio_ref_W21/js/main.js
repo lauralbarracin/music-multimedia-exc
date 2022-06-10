2821cd08a0f833e520fbeb9ffbb216579ba3b72a
Albarracin Montoya <l_albarracinmontoya@ats-hr420-dm08.local>
2022-06-10 13:54:39 -0400

    finishing loading tracks

--- audio_ref_W21/js/main.js
+++ audio_ref_W21/js/main.js
@@ -1,4 +1,26 @@
 // create a module for our project
 (() => {
+    //
+    let mainHeadline = document.querySelector('#main-headline'),
+        theAudio = document.querySelector('audio');
+        theThumbs = document.querySelectorAll('img');
+
+    //functions go in the middle -. the brains/ logic of your app 
+    function loadAudioTrack() {
+        theAudio.src = `audio/${this.dataset.trackref}.mp3` ;
+
+        theAudio.load();
+        theAudio.play();
+
+        debugger;
+    }
+
+    // event handling goes here
+    // listen for a click on the thumbnail images, load the right audio when the click happens
+    theThumbs.forEach(thumb => thumb.addEventListener('click', loadAudioTrack));
+
+
+
+    
 
 })()
